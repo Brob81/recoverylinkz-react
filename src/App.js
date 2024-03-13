f78edb8f7ec48fb7d41622ae114c85df5e4b5fdc
Brob81 <brandonrobinson81@gmail.com>
2024-03-13 01:08:53 -0500

    login page added: - forgot password - login button - retister now - social media login (currently broken)

--- src/App.js
+++ src/App.js
@@ -7,8 +7,21 @@ function App() {
       <div class="main-container">
         <div class="main-left-container"></div>
         <div class="main-right-container">
+          <div class="greeting-container">
+            <div class="greeting">Hello, Again!</div>
+            <div class="greeting-subtext">Login to your account</div>
+          </div>
           <div class="text-box">right side</div>
-          <div class="text-box">right side</div>x
+          <div class="text-box">right side</div>
+          <div class="forgot-password">Forgot Password?</div>  
+
+          <div class="login-button">Login</div>
+          <div class="register-now">Not a member? Register Now</div>
+          <div class="social-media-login">
+            <img src="./images/login-apple.png"/>
+            <img src="./images/login-facebook.png"/>
+            <img src="./images/login-google.png"/>
+          </div>
         </div>
       </div>
     </div>
@@ -16,3 +29,5 @@ function App() {
 }
 
 export default App;
+
+
